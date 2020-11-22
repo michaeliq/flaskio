25a3d9e6abd3d02f2f4a6a789e55fe0c8f7271ba
michaeliq <michaelesacol@gmail.com>
2020-11-22 11:01:49 -0500

    add new styles, add new routes and connections

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clavesecreta'
@@ -9,9 +9,25 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+@socketio.on('connect')
+def client_connected():
+    id_cli = str(request.sid)
+    print('client is connected ')
+
+@socketio.on('disconnect')
+def client_disconnected():
+    id_cli = request.sid
+    print('client is disconnected ' + str(id_cli))
+
+@socketio.on('join_personal_room')
+def join_personal_room(data):
+    id_cli = request.sid
+    room = id_cli
+    msg = data['msg']
+    send(msg, room = room)
+
 @socketio.on('message')
-def handleMessage(msg):
-    print("Message: " + msg)
+def handle_message(msg):
     send(msg, broadcast = True)
 
 
