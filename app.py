7d1a596498501e098e14bc4ca5dbe789abd36444
Josue <michaelesacol@gmail.com>
2020-11-22 10:53:52 -0500

    without changes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
+import eventlet
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'clavesecreta'
-socketio = SocketIO(app)
+socketio = SocketIO(app, message_queue='redis://', async_mode='eventlet')
 
 @app.route('/')
 def index():
