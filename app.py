439463c0f6418a223e6403010986ff4b31226353
Josue <michaelesacol@gmail.com>
2020-11-24 14:15:31 -0500

    remove message_queue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import eventlet
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'clavesecreta'
-socketio = SocketIO(app, message_queue='redis://', async_mode='eventlet')
+socketio = SocketIO(app, async_mode='eventlet')
 
 @app.route('/')
 def index():
