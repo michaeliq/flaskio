2b1c292575a78f72ddf02a0f9e1ffb731aeb3c2d
Josue <michaelesacol@gmail.com>
2020-12-03 17:09:54 -0500

    adding join_room event

--- app.py
+++ app.py
@@ -2,40 +2,56 @@ import eventlet
 eventlet.monkey_patch()
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send, emit
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
 import redis
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'clavesecreta'
 
 
-socketio = SocketIO(app, async_mode='eventlet', message_queue = "redis://", logger=True)
+socketio = SocketIO(app, async_mode='eventlet', message_queue = "redis://", logger=False)
+clients = {'quality':0,'id_users':[]}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/<string:user>')
+def private_chat(user):
+    join_room_()
+    print(request.args)
+    return render_template('index.html', user_room = user)
+
 @socketio.on('connect')
 def client_connected():
     id_cli = str(request.sid)
-    socketio.emit('connect', id_cli)
+    clients['quality'] += 1
+    clients['id_users'].append(id_cli)
+    emit('connect', id_cli)
     print('client is connected ')
 
 @socketio.on('disconnect')
 def client_disconnected():
     id_cli = request.sid
+    clients['quality'] -= 1
+    clients['id_users'].remove(id_cli)
     print('client is disconnected ' + str(id_cli))
 
-@socketio.on('join_personal_room')
-def join_personal_room(data):
-    id_cli = request.sid
-    room = id_cli
-    msg = data['msg']
-    send(msg, room = room)
+@socketio.on('join')
+def join_room_():
+    #name = request.args.get()
+    #room = data['room']
+    #join_room(room)
+    #socketio.send(name + ' has entered the room. ', room=room)
+    pass
 
 @socketio.on_error()
 def error_handler(e):
     app.logger.info(e)
+
+@socketio.on('writing')
+def is_writing(usuario):
+    emit('writing',usuario, broadcast=True, include_self=False)
 
 @socketio.on('message')
 def handle_message(msg):
--- static/script_socket.js
+++ static/script_socket.js
@@ -11,18 +11,26 @@ $('#send').on('click', function(){
     $('#myMessage').val('')
 });
 
-socket.on('connect',(id_cli)=>{
-    idUser = id_cli;
+$('#link-room').addEventListener('click',()=>{
+    socket.emit('join','user added');
 });
 
 socket.on('message', (data) => {
         if(data['sid'] == idUser ){
-            $('#mensajes').append('<li class="ml-auto p-2">' + data['name'] + ": " + data['msg'] + '</li>');
+            $('#mensajes').append('<li class="mr-auto p-2"><a class="link-room" href="#">' + data['name'] + "</a>: " + data['msg'] + '</li>');
         }else{
-            $('#mensajes').append('<li>' + data['name'] + ": " + data['msg'] + '</li>');
+            $('#mensajes').append('<li class="ml-auto p-2"><a class="link-room" href="#">' + data['name'] + "</a>: " + data['msg'] + '</li>');
         };
         $('#content_info').removeClass('d-none');
         $('#info').text(data['name']);
     });
+
+socket.on('writing',(usuario)=>{
+    console.log(usuario, ' is typing')
+});
+
+document.getElementById('myMessage').addEventListener('keypress', ()=>{
+    socket.emit('writing',nombreUsuario);
+});
 
 
